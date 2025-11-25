90b0c804ca9ed6d43ed31057047c5d60f4e104af
Ryan <ryan@candidstudios.net>
2025-11-25 15:34:45 -0500

    Update Keycloak URL to admin.candidstudios.net

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,8 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 // Keycloak configuration - using master realm
+// Keycloak server is at admin.candidstudios.net
 const keycloakConfig = {
-  url: 'https://login.candidstudios.net',
+  url: 'https://admin.candidstudios.net',
   realm: 'master',
   clientId: 'candid-dash',
 };
